5dfa9403af55d5b5ee1772e32c23d65fe5dc567a
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-11 23:28:10 +0300

    dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,32 +1,11 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
   
 const DashboardPage = () =>  {
   return (
     <div>
       <p className="">DashboardPage (protected)</p>
       <UserButton afterSignOutUrl="/"/>
-      <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-
     </div>
   )
 }

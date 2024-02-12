24c2640e3c244e51883eb60d66108dd8ece5a9c9
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-13 00:33:16 +0300

    sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { cn } from '@/lib/utils'
+import { Montserrat } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const montserrat = Montserrat(
+    { weight: '600',
+    subsets: ['latin']})
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+           <div className="px-3 py-2 flex-1">
+            <Link href="/dashboard" className='flex items-center pl-3 mb-14'>
+                <div className='relative w-8 h-8 mr-4'>
+               <Image src="/log.png" fill objectFit="contain" alt="logo" />
+                </div>
+                <h1 className={cn("text-2xl font-bold", montserrat.className)}>Aurora</h1>
+            </Link>
+           </div>
+        </div>
+    )
+}
+
+export default Sidebar

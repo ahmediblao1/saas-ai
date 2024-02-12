bd5b7ab667862306474fb840c8e0bd83405006cb
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-12 23:58:32 +0300

    dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { UserButton } from "@clerk/nextjs"
+
   
 const DashboardPage = () =>  {
   return (
     <div>
       <p className="">DashboardPage (protected)</p>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }

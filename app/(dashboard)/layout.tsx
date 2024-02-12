499bd932d94263b3e8e0b777fac1150278b8d245
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-12 23:25:49 +0300

    dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar"
 
 const DashboardLayout =  ({ 
     children 
@@ -6,10 +7,18 @@ const DashboardLayout =  ({
 }) => {
     return(
 <div className="h-full relative">
-<div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0
+<div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0
 z-[80] bg-gray-900">
-
+<div className="text-white">
+    HEllo sidebar
 </div>
+</div>
+<main className="md:pl-72">
+    <Navbar />
+    {children}
+</main>
 </div>
     )
 }
+
+export default DashboardLayout
---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
+
+const Navbar = () => {
+    return (
+        <div className="flex items-center p-4">
+            <Button variant="ghost" size="icon" className="md:hidden">
+                <Menu/>
+            </Button>
+        </div>
+    )
+}
+export default Navbar

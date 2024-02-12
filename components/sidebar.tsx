982f4c625c83f78260a7188926b99e88853cb0e8
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-13 01:02:21 +0300

    sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { cn } from '@/lib/utils'
-import { LayoutDashboard } from 'lucide-react'
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from 'lucide-react'
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -15,6 +15,41 @@ const routes = [
         icon: LayoutDashboard,
         href: '/dashboard',
         color: "text-sky-500"
+    },
+    {
+        label: 'Conversation',
+        icon: MessageSquare,
+        href: '/conversation',
+        color: "text-violet-500"
+    },
+    {
+        label: 'Image Generation',
+        icon: ImageIcon,
+        href: '/image',
+        color: "text-pink-700"
+    },
+    {
+        label: 'Video Generation',
+        icon: VideoIcon,
+        href: '/cideo',
+        color: "text-orange-700"
+    },
+    {
+        label: 'Music Generation',
+        icon: MusicIcon,
+        href: '/musci',
+        color: "text-emerald-500"
+    },
+    {
+        label: 'Code Generation',
+        icon: Code,
+        href: '/code',
+        color: "text-green-700"
+    },
+    {
+        label: 'Settings',
+        icon: Settings,
+        href: '/settings',
     },
     
 ]
@@ -33,10 +68,15 @@ const Sidebar = () => {
             </Link>
             <div className='space-y-1'>
             {routes.map((route, index) => (
-                <Link href={route.href} key={index}>
-                    <div className='flex items-center flex-1'>
-                    {<route.icon className={cn("h-5 w-5 mr-3", route.color)} />}
-                    </div>
+                <Link
+                 href={route.href}
+                key={index}
+                className={cn("flex items-center px-3 py-2 rounded-md", "hover:bg-[#1F2937]")}
+                >
+                <div className='flex items-center flex-1'>
+                {<route.icon className={cn("h-5 w-5 mr-3", route.color)} />}
+                {route.label}
+                </div>
                 </Link>
             ))}
             </div>

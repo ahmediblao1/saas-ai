f7d6ba4f3b1cd2befa03f02acaa0d4b82c7aaf2d
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-14 23:53:26 +0300

    coversation ai

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -49,13 +49,12 @@ const tools = [
   },
 ]
 
-  
 const DashboardPage = () =>  {
 const router = useRouter()
 
   return (
     <div>
-      <div className="mb-8 space-y-4">
+      <div className="mb-8 space-y-2">
         <h2 className="text-2xl md:text-4xl font-bold text-center">Explore the power of ai</h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Aurora your nest ai firend - experinece the power of ai

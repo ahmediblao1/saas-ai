840f90604becf439e5a53eb81089998394c32f66
cod4r iblao <ahmediblao53@gmail.com>
2024-02-26 18:42:41 +0300

    Merge remote changes

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,21 +1,32 @@
 "use client"
 
+import { useProModal } from "@/hooks/use-pro-modal"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog"
+import { Badge } from "./ui/badge"
+
 
 export const ProModal = () => {
+
+    const proModal = useProModal()
+
 return(
-    <div>
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="fixed inset-0 bg-black opacity-60"></div>
-        <div className="relative bg-white rounded-lg w-1/2">
-        <div className="flex items-center justify-between p-4 border-b">
-            <h1 className="text-lg font-bold">Modal</h1>
-            <button className="text-lg font-bold" onClick={() => {}}>X</button>
-        </div>
-        <div className="p-4">
-            <p>Modal Content</p>
-        </div>
-        </div>
-    </div>
-    </div>
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+    <DialogContent>
+        <DialogHeader>
+            <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                <div className="flex items-center gap-x-2 font-bold py-1">
+                Upgrade to Aurora
+                <Badge variant="premium" className="u uppercase text-sm py-1">
+                  Pro
+                </Badge>
+                </div>
+            </DialogTitle>
+        </DialogHeader>
+    </DialogContent>
+    </Dialog>
 )
+}
+
+function useProModalStore(arg0: (state: any) => any) {
+    throw new Error("Function not implemented.")
 }

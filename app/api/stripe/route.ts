825136cc1a7b0f83265b29ea57bbd3e2778a4053
cod4r iblao <ahmediblao53@gmail.com>
2024-02-29 00:38:10 +0300

    pro modal

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -5,11 +5,30 @@ import prismadb from "@/lib/prismadb"
 import { stripe } from "@/lib/stripe"
 import { absoluteUrl } from "@/lib/utils"
 
-const settingUrl = absoluteUrl("/settings")
+const settinsgUrl = absoluteUrl("/settings")
 
 export async function GET(){
 try {
-    
+    const { userId } = auth()
+    const user = await currentUser()
+
+    if (!user) {
+        return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+        where: { userId: userId ?? "" }
+    })
+
+    if(userSubscription && userSubscription.stripeCustomerId){
+        const stripeSession = await stripe.billingPortal.sessions.create({
+            customer: userSubscription.stripeCustomerId,
+            return_url: settinsgUrl
+        })
+    }
+
+
+
 } catch (error) {
     console.error("[ STRIPE_ERROR]", error)
     return new NextResponse("Internal Server Error", { status: 500 })

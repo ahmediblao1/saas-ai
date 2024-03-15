fa3044b3d126dd563825afcf6c8e08d736b5249c
cod4r iblao <ahmediblao53@gmail.com>
2024-03-16 00:41:41 +0300

    pro model

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -135,9 +135,9 @@ const [messages, setMessages] = useState<OpenAI.Chat.ChatCompletionMessage[]>([]
                             <div
                              key={message.content}
                              className={cn("p-8 w-full flex items-start gap-x-4 rounded-lg",
-                             message.role === "user" ? "bg-muted border border-black/10 " : "bg-white")}
+                             message.role === "assistant" ? "bg-muted border border-black/10 " : "bg-white")}
                              > 
-                            {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                            {message.role === "assistant" ? <UserAvatar /> : <BotAvatar />}
 
                                 <ReactMarkdown 
                                 components={{

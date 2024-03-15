b8144b4479ad33696c4c80a3bac4e050802f2a73
cod4r iblao <ahmediblao53@gmail.com>
2024-03-15 23:42:46 +0300

    pro model

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,14 +2,16 @@ import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNav } from "@/components/landing-nav";
 import { NavbarDemo } from "@/components/landing_navbar";
+// import { TabsDemo } from "@/components/sticky-scroll";
 
 
 const LandingPage = () => {
   return (
     <div className="h-full">
-      <NavbarDemo />
+      {/* <NavbarDemo /> */}
       <LandingNav />
       <LandingHero />
+      {/* <TabsDemo /> */}
       <LandingContent />
     </div>
   );
